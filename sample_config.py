16487808c4465853aef63b2d8be40d7033726444
Arden Liao <liao.arden@gmail.com>
2013-09-27 16:44:35 -0700

    add sample config.py
    tentative format with globals, should change to become a python obj.
    will do for now

---
+++ sample_config.py
@@ -0,0 +1,26 @@
+# sample_config.py contains basic defines for locations of repository 
+# information as well as presets for organizations and input files.
+# TODO: Make this into a python object, for now just use globals
+# TODO: Please copy or rename this file as config.py with your own custom settings
+#       for use with the scripts
+
+students        = "test1.csv"
+pairs           = "pairs.csv"
+org             = "UCSD-CSE-100"
+scratchRepoDir  = "/home/linux/ieng6/oce/78/arliao/Github/scratchRepos"
+prototypesDir   = "/home/linux/ieng6/oce/78/arliao/Github/prototypes"
+
+def getStudentsFile():
+    return students
+
+def getPairsFile():
+    return pairs
+
+def getOrgName():
+    return org
+
+def getScratchRepoDir():
+    return scratchRepoDir
+	
+def getPrototypeDir():
+    return prototypesDir
