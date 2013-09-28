4d77c079753b5ef7b7eae73ea2e59d71091d0fd2
Arden Liao <liao.arden@gmail.com>
2013-09-28 01:47:10 -0700

    init script modifies default config entries
    will modify it per user on. Config entries can be customized later, will
    need to ensure directories are created at runtime (maybe let main script
    handle that)

--- initRepo.sh
+++ initRepo.sh
@@ -4,8 +4,25 @@
 git clone git@github.com:jacquev6/PyGithub.git
 
 #create the config file
-git cp sample_config.py config.py
+cp sample_config.py config.py
 
 #intialize the config file values to some default values
 #makes the scratch, submission, and logs directories if they don't already exist
-#TODO
+sed -i "s,USER,$USER,g" config.py
+
+SCRATCHREPODIR=`python -c 'import config; dir = config.getScratchRepoDir(); print(dir)'`
+SUBMISSIONSDIR=`python -c 'import config; dir = config.getLabSubmissionsDir(); print(dir)'`
+SCRIPTSLOGSDIR=`python -c 'import config; dir = config.getScriptsLogsDir(); print(dir)'`
+
+echo "Creating directories..."
+if [ ! -d "${SCRATCHREPODIR}" ]; then
+	mkdir ${SCRATCHREPODIR}
+fi
+
+if [ ! -d "${SUBMISSIONSDIR}" ]; then
+	mkdir ${SUBMISSIONSDIR}
+fi
+
+if [ ! -d "${SCRIPTSLOGSDIR}" ]; then
+	mkdir ${SCRIPTSLOGSDIR}
+fi
