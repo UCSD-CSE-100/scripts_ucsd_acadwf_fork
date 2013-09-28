c42e9dccb528567a7a8cd602a59d48bf404ba529
Arden Liao <liao.arden@gmail.com>
2013-09-27 22:27:30 -0700

    basic checking for python script success
    not sure if this will work due to how python scripts bail out. will
    refine

--- addStudentsCreateRepos.sh
+++ addStudentsCreateRepos.sh
@@ -3,7 +3,7 @@
 if [ $# -ne 2 ] ; then
 	echo "Useage: addStudentsCreateRepos.sh <Github Username> <Labnum>"
 	echo "ex    : ./addStudentsCreateRepos.sh testaccount lab01"
-        exit
+    exit
 fi
 
 #log the latest repo creation
@@ -15,10 +15,42 @@ fi
 user="${1}"
 lab="${2}"
 
+echo "Adding Students to teams."
 ./addStudentsToTeams.py -u ${user} >> ${repoLogs}/${lab}_AddingStudentsAndTeams
-./createPairTeams.py -u ${user} >> ${repoLogs}/${lab}_AddingStudentsAndTeams
-./createLabRepo.py -u ${user} ${lab} >> ${repoLogs}/${lab}_CreatingRepos
-./createLabRepoForPairs.py -u ${user} ${lab} >> ${repoLogs}/${lab}_CreatingRepos
-./pushFilesToRepo.py -u ${user} ${lab} >> ${repoLogs}/${lab}_PopulateRepos
+if [ $? -ne 0 ]; then
+	echo -e "Did not properly add all students to organization\n"
+	exit
+fi
 
-echo "Check ${repoLogs} for status on operations"
+echo "Creating Paired teams"
+./createPairTeams.py -u ${user} >> ${repoLogs}/${lab}_AddingStudentsAndTeams
+if [ $? -ne 0 ]; then
+	echo -e "Did not properly create all teams\n"
+	exit
+fi
+
+echo "Creating individual repos"
+./createLabRepo.py -u ${user} ${lab} >> ${repoLogs}/${lab}_CreatingRepos
+if [ $? -ne 0 ]; then
+    echo -e "Did not create all individual repos\n"
+	exit
+fi
+
+echo "Creating pair repos"
+./createLabRepoForPairs.py -u ${user} ${lab} >> ${repoLogs}/${lab}_CreatingRepos
+if [ $? -ne 0 ]; then
+	echo -e "Did not create all team repos\n"
+	exit
+fi
+
+echo "Pushing files to created repos"
+./pushFilesToRepo.py -u ${user} ${lab} >> ${repoLogs}/${lab}_PopulateRepos
+if [ $? -ne 0 ]; then
+    echo -e "Could not push files to all repos\n"
+	exit
+fi
+
+echo "All operations return okay. Check ${repoLogs} for detailed status on operations"
+
+#cleanup
+rm -f *.pyc
