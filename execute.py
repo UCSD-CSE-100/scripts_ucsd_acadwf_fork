f046a00127c4f5d4dd4059528b78ef47aab9700e
Arden Liao <liao.arden@gmail.com>
2013-09-29 17:36:09 -0700

    added some error checking

--- execute.py
+++ execute.py
@@ -5,10 +5,17 @@ import sys
 
 print(len(sys.argv))
 
+if (len(sys.argv) < 4):
+    print("Not enough args supplied!")
+	sys.exit(1)
+
 script    = sys.argv[1]
 argstring = sys.argv[2]
 password  = sys.argv[3]
 
+print(script)
+print(argstring)
+print(password)
 
 # child = pexpect.spawn('./'+ script + ' ' + argstring)
 # child.expect('Password:')
