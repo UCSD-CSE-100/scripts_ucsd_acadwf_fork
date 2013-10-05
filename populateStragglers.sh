f80dc43451b2dcd17cda89e016ffea6ef4464673
Arden Liao <cs100e5@ieng6-201.ucsd.edu>
2013-10-05 13:12:04 -0700

    Forgot to stage my populate changes

--- populateStragglers.sh
+++ populateStragglers.sh
@@ -8,6 +8,9 @@ protodir=`echo ${protodir}$1`
 scratchDir=`python -c 'import config;print(config.getScratchRepoDir())'`
 
 while read line; do
-echo "${line}"
+   repoName=`echo ${line} | awk -F',' '{print $1}'`
+   repoUrl=`echo ${line} | awk -F',' '{print $2}'`
+   ./populateRepo.sh $repoName $repoUrl $protodir $scratchDir
+   echo "RepoName: ${repoName} ${repoUrl} ${protodir} ${scratchDir}"
 
 done < straglers.txt
