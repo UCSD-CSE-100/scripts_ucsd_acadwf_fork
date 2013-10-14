00a47d318aefd189296330ae881a60df32f7062c
Arden Liao <liao.arden@gmail.com>
2013-10-13 20:46:12 -0700

    adding in logic for finding stragglers

--- Grading/pullStragglers.sh
+++ Grading/pullStragglers.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ $# -lt 1 ]; then
+    echo "Usage: pullStragglers.sh labNum"
+    exit 1
+fi
+
 tutors=("victor_alor" "victoria_do" "leta_he" "arden_liao" "ryan_liao" "scott_lin" "michael_luo" "dong_nam")
 
 declare -i counter
@@ -13,11 +18,20 @@ pulled="${submissionsDir}students_pulled"
 echo "${pulled}"
 while read line; do
     let curr=counter%8
-    echo "$line"
-    echo "${tutors[$curr]}"
-    if [ $? -eq 0 ]; then
-        let counter=counter+1
+    githubid=`echo ${line} | awk -F',' '{print $4}'`
+    isPulled=`grep "${githubid}" ${pulled}`
+    if [ -z "${isPulled}" ]; then
+        currTutor="${tutors[$curr]}"
+        repoName="$1_${githubid}"
+        repoUrl="git@github.com:UCSD-CSE-100/${repoName}.git"
+        
+        echo "${repoName}, ${repoUrl}, ${currTutor}"
+        
+        if [ $? -eq 0 ]; then
+            let counter=counter+1
+        fi
     fi
+    
 done < temp
 
 rm -f temp
