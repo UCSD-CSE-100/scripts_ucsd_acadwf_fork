8f3f8ae9f168a80ee4002925047cfa16cddf4c15
Arden Liao <cs100e5@ieng6-201.ucsd.edu>
2013-11-02 01:02:58 -0700

    adding in capability for detecting pairs that have done no work

--- remove_unused.sh
+++ remove_unused.sh
@@ -5,20 +5,29 @@ if [ $# -ne 1 ]; then
     exit 0
 fi
 
-awk 'NR>1' ../students_list.csv > temp_students
+awk 'NR>1' students_list.csv > temp_students
+awk 'NR>1' "${1}Pairs.csv" > temp_pairs
 
 scratchdir=`python -c 'import config; print(config.getScratchRepoDir())'`
+cd ${scratchdir}
 
 while read line; do
-    gitid=`echo ${line} | awk -F',' '{print $4}'`
-    repo="${1}_gitid"
+    gitid=`echo ${line} | awk -F',' '{print $4}' | tr '[:upper:]' '[:lower:]' `
+    repo="${1}_${gitid}"
     git clone git@github.com:UCSD-CSE-100/${repo}
     cd ${repo}
     
     worked=`diff BST.hpp $PUBLIC/P1/BST.hpp`
-    if [ ! -z "${worked}" ]; then
-        echo "${repo}" | tee -a not_worked
+    if [ -z "${worked}" ]; then
+        echo "${repo}" | tee -a ${scratchdir}not_worked
     fi
-done < temp_students
 
-rm -f temp_students
+    cd ${scratchdir}; rm -rf ${repo}
+done < ~/deploy/temp_students
+
+while read line; do
+
+done < ~/deploy/temp_pairs
+
+cd ~/deploy
+rm -f temp_students temp_pairs
