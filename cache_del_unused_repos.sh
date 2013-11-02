1bc41f6610500aec2ff763caf4605fb1551f88bd
Arden Liao <liao.arden@gmail.com>
2013-11-02 02:57:17 -0700

    added bundling code
    cache a local copy of their repositories before deleting

--- cache_del_unused_repos.sh
+++ cache_del_unused_repos.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
 
-echo -n "Username: "
-read username
+if [ $# -ne 1 ]; then
+    echo "Usage: cache_del_unused_repos.sh CACHE|DEL"
+    exit 0
+fi
 
-echo -n "OAuth/Password: "
-read -s pwd
-echo
+if [ "$1" == "CACHE" ]; then
+    scratchdir=`python -c 'import config; print(config.getScratchRepoDir())'`
+    homedir=`pwd`
+    
+    cd ${scratchdir}
+    while read line; do
+        bundle=`echo ${line} | awk -F'_' '{print $1}'`
+        git clone git@github.com:UCSD-CSE-100/${repo}
+        cd ${line}
+        
+        git bundle create ${line}.bundle master
+        zip ${scratchdir}${bundle}_cache.zip ${line}.bundle
+        
+        cd ${scratchdir}; rm -rf ${line}
+    done < ${homedir}/not_worked
 
-while read line; do
-    echo "Removing REPO ${line}"
-    #curl -X DELETE -u ${username}:${pwd} https://api.github.com/repos/UCSD-CSE-100/${line}
-done < not_worked
+elif [ "$1" == "DEL" ]; then
+    echo -n "Username: "
+    read username
+
+    echo -n "OAuth/Password: "
+    read -s pwd
+    echo
+
+    while read line; do
+        echo "Removing REPO ${line}"
+        #curl -X DELETE -u ${username}:${pwd} https://api.github.com/repos/UCSD-CSE-100/${line}
+    done < not_worked
+
+fi
+
 
