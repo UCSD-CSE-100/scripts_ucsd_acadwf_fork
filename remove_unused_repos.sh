431fb2ab121947811ff1638776f0760d3a333811
Arden Liao <liao.arden@gmail.com>
2013-11-02 02:11:48 -0700

    adding removal of repos scripts

---
+++ remove_unused_repos.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo -n "Username: "
+read username
+
+echo -n "OAuth/Password: "
+read -s pwd
+echo
+
+while read line; do
+    echo "Removing REPO ${line}"
+    #curl -X DELTE -u ${username}:${pwd} https://api.github.com/repos/UCSD-CSE-100/${line}
+done < not_worked
+
