62be1718008ddbee980ceab114fd3d646f2f24c7
Arden Liao <liao.arden@gmail.com>
2013-11-02 01:16:11 -0700

    moving check to function
    commenting out pair code for now

--- remove_unused.sh
+++ remove_unused.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+checkWork()
+{
+    git clone git@github.com:UCSD-CSE-100/${2}
+    cd ${2}
+    
+    if [ "${1}" == "P1" ]; then
+        worked=`diff BST.hpp $PUBLIC/P1/BST.hpp`
+    elif [ "${1}" == "P2" ]; then
+        worked=`[ -f "RST.hpp" ] && echo "hello"`
+    fi
+    
+    if [ -z "${worked}" ]; then
+        echo "${2}" | tee -a ${scratchdir}not_worked
+    fi
+}
+
 if [ $# -ne 1 ]; then
     echo "Args P1 or P2"
     exit 0
@@ -14,20 +30,27 @@ cd ${scratchdir}
 while read line; do
     gitid=`echo ${line} | awk -F',' '{print $4}' | tr '[:upper:]' '[:lower:]' `
     repo="${1}_${gitid}"
-    git clone git@github.com:UCSD-CSE-100/${repo}
-    cd ${repo}
     
-    worked=`diff BST.hpp $PUBLIC/P1/BST.hpp`
-    if [ -z "${worked}" ]; then
-        echo "${repo}" | tee -a ${scratchdir}not_worked
-    fi
-
+    checkWork ${1} ${repo}
+    
     cd ${scratchdir}; rm -rf ${repo}
+    
 done < ~/deploy/temp_students
 
-while read line; do
+# cd ${scratchdir}
+# while read line; do
+    # gitid1=`echo "${line}" | awk -F',' '{print $1}' | tr '[:upper:]' '[:lower:]'`
+    # gitid2=`echo "${line}" | awk -F',' '{print $2}' | tr '[:upper:]' '[:lower:]'`
+    
+    # repo="${1}_Pair_${gitid1}_${gitid2}"
+    # exists=`git ls-remote git@github.com:UCSD-CSE-100/${repo} 2>&1 | grep "ERROR"`
+    # if [ ! -z "${exists}" ] && repo="${1}_Pair_${gitid2}_${gitid1}"
+    
+    # checkWork ${1} ${repo}
+    
+    # cd ${scratchdir}; rm -rf ${repo}
 
-done < ~/deploy/temp_pairs
+# done < ~/deploy/temp_pairs
 
 cd ~/deploy
 rm -f temp_students temp_pairs
