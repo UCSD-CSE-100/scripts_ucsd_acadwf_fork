582882121c5aee2c8640e269066d0e9b5704ce0b
Arden Liao <cs100e5@ieng6-201.ucsd.edu>
2013-11-02 02:38:10 -0700

    renamed remove_unused_repos to cache_del_unused_repos.sh

--- cache_del_unused_repos.sh
+++ cache_del_unused_repos.sh
@@ -9,6 +9,6 @@ echo
 
 while read line; do
     echo "Removing REPO ${line}"
-    #curl -X DELTE -u ${username}:${pwd} https://api.github.com/repos/UCSD-CSE-100/${line}
+    #curl -X DELETE -u ${username}:${pwd} https://api.github.com/repos/UCSD-CSE-100/${line}
 done < not_worked
 
