c33fd4c7320f586c0f4dcb8d86b645ffa76e0033
Arden Liao <liao.arden@gmail.com>
2013-11-03 13:47:35 -0800

    added more fields to csv

--- Grading/create_csvs.sh
+++ Grading/create_csvs.sh
@@ -6,7 +6,11 @@ bundle_dir="../../labSubmissions"
 
 for tutor in "${tutors[@]}"; do
     unzip -l ${bundle_dir}/${tutor}.zip | egrep "*.tar.gz" | awk '{print $4}' > temp
-    echo "Tutor,Student,GithubID,PID,Pair,Ontime,Late_One,Late_Two,Comments" > ${bundle_dir}/${tutor}.csv
+    echo -n "Tutor,Student,GithubID,PID,Pair,Checkpoint," > ${bundle_dir}/${tutor}.csv
+    echo -n "Ontime_RST,Ontime_RST_Style,OnTime_Benchtree," >> ${bundle_dir}/${tutor}.csv
+    echo -n "Late1_RST,Late1_RST_Style,Late1_Benchtree," >> ${bundle_dir}/${tutor}.csv
+    echo -n "Late2_RST,Late2_RST_Style,Late2_Benchtree," >> ${bundle_dir}/${tutor}.csv
+    echo "Benchtree_PDF, Comments" >> ${bundle_dir}/${tutor}.csv
     echo "${tutor}"
     echo "---------------------------------------------"
     while read student; do
@@ -17,21 +21,24 @@ for tutor in "${tutors[@]}"; do
             pid1=`echo "${entry1}" | awk -F',' '{print $6}'`
             fName1=`echo "${entry1}" | awk -F',' '{print $2}'`
             lName1=`echo "${entry1}" | awk -F',' '{print $3}'`
-            echo "${tutor},${fName1} ${lName1},${student1},${pid1},YES,,,," >> ${bundle_dir}/${tutor}.csv
+            echo -n "${tutor},${fName1} ${lName1},${student1},${pid1},YES," >> ${bundle_dir}/${tutor}.csv
+            echo "0,0,0,0,0,0,0,0,0,0," >> ${bundle_dir}/${tutor}.csv
             
             student2=`echo "${student}" | tr '.' '_' | awk -F'_' '{print $4}'`
             entry2=`grep -i "${student2}" ../students_list.csv`
             pid2=`echo "${entry2}" | awk -F',' '{print $6}'`
             fName2=`echo "${entry2}" | awk -F',' '{print $2}'`
             lName2=`echo "${entry2}" | awk -F',' '{print $3}'`
-            echo "${tutor},${fName2} ${lName2},${student2},${pid2},YES,,,," >> ${bundle_dir}/${tutor}.csv
+            echo -n "${tutor},${fName1} ${lName1},${student1},${pid1},YES," >> ${bundle_dir}/${tutor}.csv
+            echo "0,0,0,0,0,0,0,0,0,0," >> ${bundle_dir}/${tutor}.csv
             
         else
             entry1=`grep -i "${student1}" ../students_list.csv`
             pid1=`echo "${entry1}" | awk -F',' '{print $6}'`
             fName1=`echo "${entry1}" | awk -F',' '{print $2}'`
             lName1=`echo "${entry1}" | awk -F',' '{print $3}'`
-            echo "${tutor},${fName1} ${lName1},${student1},${pid1},NO,,,," >> ${bundle_dir}/${tutor}.csv
+            echo -n "${tutor},${fName1} ${lName1},${student1},${pid1},NO," >> ${bundle_dir}/${tutor}.csv
+            echo "0,0,0,0,0,0,0,0,0,0," >> ${bundle_dir}/${tutor}.csv
         fi
     done < temp
     zip ${bundle_dir}/${tutor}.zip ${bundle_dir}/${tutor}.csv
