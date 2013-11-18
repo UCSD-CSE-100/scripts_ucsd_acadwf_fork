db5ec2eacab49f3cbf4ac3e5706339dae441378a
Arden Liao <liao.arden@gmail.com>
2013-11-18 14:15:20 -0800

    fleshing out pulling for P3

--- Grading/pullRepo.sh
+++ Grading/pullRepo.sh
@@ -39,7 +39,7 @@ else
 fi
 if [ -f "compress.cpp" ] && [ -f "uncompress.cpp"  ]; then
     tar -cvf ../${1}_checkpoint.tar compress.cpp uncompress.cpp HCNode.*pp HCTree.*pp
-    if [ -a "BitInputStream.cpp" ] && [ -a "BitOutputStream.cpp" ]; then
+    if [ -s "BitInputStream.cpp" ] && [ -s "BitOutputStream.cpp" ]; then
         tar -rvf ../${1}_checkpoint.tar BitInputStream.*pp BitOutStream.*pp
     fi
 fi
@@ -47,7 +47,7 @@ git checkout master
 git branch -d checkpoint
 
 #check if final submission exists, unless we are ignoring final submission
-exists=`git rev-list -n 1 --before="10/25/2013 20:15" --grep="FINAL" master`
+exists=`git rev-list -n 1 --before="10/25/2013 20:15" --grep="final" -i master`
 if [ -z "${exists}" ]; then
     revision=`git rev-list -n 1 --before="10/25/2013 20:15" master`
     git checkout ${revision} -b ontime
@@ -55,39 +55,50 @@ else
     ontime_check="TRUE"
     git checkout ${exists} -b ontime
 fi
-if [ -f "BST.hpp" ]; then
-    tar -cvf ../${1}_ontime.tar BST*.hpp RST.hpp benchtree.cpp countint.*pp
+if [ -f "compress.cpp" ] && [ -f "uncompress.cpp" ]; then
+    tar -cvf ../${1}_ontime.tar *.cpp *.hpp
 fi
 git checkout master
 git branch -d ontime
 
 #check for late submission day one, always get latest commit
-lateOne=`git rev-list -n 1 --before="10/26/2013 20:15" --after="10/25/2013 20:15" master`
+unset revision
+lateOne=`git rev-list -n 1 --before="11/19/2013 20:15" --after="11/18/2013 20:15" --grep="final" -i master`
 if [ ! -z "${lateOne}" ]; then
-   git checkout ${lateOne} -b lateone
-   if [ -f "BST.hpp" ]; then
-      tar -cvf ../${1}_lateone.tar BST*.hpp RST.hpp benchtree.cpp countint.*pp
+    revision=${lateOne}
+else if [ -z "${ontime_check}"]; then
+    revision=`git rev-list -n 1 --before="11/19/2013 20:15" --after="11/18/2013 20:15" master`
+fi
+
+if [ ! -z "${revision}" ]; then
+    git checkout ${revision} -b lateone
+   if [ -f "compress.cpp" ] && [ -f "uncompress.cpp" ]; then
+      tar -cvf ../${1}_lateone.tar *.hpp *.cpp
    fi
    git checkout master
    git branch -d lateone
 fi
 
 #check for late submission day two, always get latest commit
-lateTwo=`git rev-list -n 1 --before="10/27/2013 20:15" --after="10/26/2013 20:15" master`
+lateTwo=`git rev-list -n 1 --before="11/20/2013 20:15" --after="11/19/2013 20:15" --grep="final" -i master`
 if [ ! -z "${lateTwo}" ]; then
-   git checkout ${lateTwo} -b latetwo
-   if [ -f "BST.hpp" ]; then
-      tar -cvf ../${1}_latetwo.tar BST*.hpp RST.hpp benchtree.cpp countint.*pp
+    revision=${lateTwo}
+else if [ -z "${ontime_check}"]; then
+    revision=`git rev-list -n 1 --before="11/20/2013 20:15" --after="11/19/2013 20:15" master`
+fi
+
+if [ ! -z "${revision}" ]; then
+    git checkout ${revision} -b latetwo
+   if [ -f "compress.cpp" ] && [ -f "uncompress.cpp" ]; then
+      tar -cvf ../${1}_latetwo.tar *.hpp *.cpp
    fi
    git checkout master
    git branch -d latetwo
 fi
 
-cp *.pdf ../.
-
 cd ..
 if [ -f "${1}_lateone.tar" ] || [ -f "${1}_ontime.tar" ] || [ -f "${1}_latetwo.tar}" ]; then
-    tar --ignore-failed-read -czvf ${1}.tar.gz *.tar *.pdf
+    tar --ignore-failed-read -czvf ${1}.tar.gz *.tar
     zip ${graderZip} ${1}.tar.gz
 fi
 
