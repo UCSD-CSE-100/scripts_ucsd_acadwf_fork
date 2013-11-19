b1a77feba0f984cbb12ab945f2f5d67b80041487
Arden Liao <liao.arden@gmail.com>
2013-11-19 02:51:00 -0800

    fixed zip behavior

--- Grading/zip_csvs.sh
+++ Grading/zip_csvs.sh
@@ -3,6 +3,8 @@
 submissionsDir=`python -c 'import sys; sys.path.append(".."); import config; print(config.getLabSubmissionsDir())'`
 tutors=("victor_alor" "victoria_do" "leta_he" "arden_liao" "ryan_liao" "scott_lin" "michael_luo" "dong_nam")
 
+cd ${submissionsDir}
+
 for tutor in "${tutors[@]}"; do
-    zip ${submissionsDir}${tutor}.zip ${submissionsDir}${tutor}.csv
+    zip ${tutor}.zip ${tutor}.csv
 done
