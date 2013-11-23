6c4605444d368031e9707eef263114f1cb053fb0
Arden Liao <liao.arden@gmail.com>
2013-11-23 04:15:26 -0800

    fleshing out test read_pairs

--- Testing/read_pairs.py
+++ Testing/read_pairs.py
@@ -8,16 +8,20 @@ sys.path.append("../PyGithub")
 sys.path.append("..")
 
 import config
-
-pairFile = config.getPairFile()
-
 pairs = {}
 
-with open(config.getPairFile(), 'rb') as pairFile:
+with open("../" + config.getPairsFile(), 'rb') as pairFile:
     pair_reader = csv.DictReader(pairFile)
     for line in pair_reader:
         pairs[line['Partner1_GithubID']] = line['Partner2_GithubID']
+        pairs[line['Partner2_GithubID']] = line['Partner1_GithubID']
 
-print pairs
+count = 0
+with open("../" + config.get) as studentsFile:
+    student_reader = csv.DictReader(studentsFile)
+    for line in student_reader:
+        if line['github userid'] in pairs:
+            print "{} {} is in a pair!".format(line['First Name'], line['Last Name']) 
+            count += 1
 
-    
+print "There were {0} pairs!".format(count/2)
