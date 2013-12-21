d955cdf3d1a4339fd621d869d077a3e157708acd
Arden Liao <liao.arden@gmail.com>
2013-12-20 21:49:15 -0800

    added in logic to delete all student teams for a github organization

--- utility/deleteteam.py
+++ utility/deleteteam.py
@@ -5,6 +5,7 @@
 """
 
 #Python STL
+import getpass
 import logging
 import re
 import subprocess
@@ -19,13 +20,24 @@ except:
 #importing user defined libraries
 sys.path.append("..")
 import argparse
+import config
 
 GET_TEAMS   = "https://api.github.com/orgs/{_org}/teams"
 DELETE_TEAM = "https://api.github.com/teams/{_id}"
 
 def main():
-    return 0
+    class_org = config.getOrgname()
 
+    username = raw_input("Username: ")
+    password = getpass.getpass()
+
+    credentials = (username, password)
+    teams       = get_teams(class_org, credentials)
+
+    for team in teams:
+        if (re.search("\A(Student_)", team[0]) != None) or \
+           (re.search("\A(Pair_)", team[0]) != None):
+            delete_team(team, credentials)
 
 def get_teams(org, credentials):
     request_url = GET_TEAMS.format(_org=org)
@@ -42,14 +54,21 @@ def get_teams(org, credentials):
 
 def delete_team(team, credentials):
     request_url = DELETE_TEAM.format(_id=team[1])
+    deleted = False
+
     try:
         req = requests.delete(request_url, credentials)
-        if (req.status_code != requests.codes.nocontent):
-            logging.error("Could not delete team {0} with id {1}".format(
-                          team[0], team[1]))
+        deleted = (req.status_code == requests.codes.nocontent):
     except NameError
         logging.info("Requests not imported, using subprocess")
+        repo_proc = subprocess.Popen(["curl", "-X", "DELETE", "-u",
+                                      credentials[0]+":"+credentials[1],
+                                      request_url])
+        deleted = (repo_proc.wait() == 0):
 
+    if not deleted:
+        logging.error("Could not delete team {0} with id {1}".format(team[0],
+                                                                     team[1]))
 
 if __name__ == "__main__":
     main()
