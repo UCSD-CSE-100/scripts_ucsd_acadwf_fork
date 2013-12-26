31533152f030b3ec3280833f35b661fad23be20e
Arden Liao <liao.arden@gmail.com>
2013-12-26 01:22:13 -0800

    streamlined pulling repo to work with dict of provided args rather than explicit args

--- utility/pullrepo.py
+++ utility/pullrepo.py
@@ -4,19 +4,16 @@
     Author: Arden Liao (ardentsonata)
 """
 
+import logging
 import subprocess
-import sys
-
-sys.path.append("..")
-import config
 
 SOLO_NAME = "{_lab}_{_ghid}"
 PAIR_NAME = "{_lab}_Pair_{_ghid1}_{_ghid2}"
 
 REPO_URL  = "git@github.com/{_org}/{_repo}"
 
-def pull_repo(lab, org, ghid, ghid2=None, tutor, duedate, chkpoint=None,
-              dbglog):
+def pull_repo_depre(lab, org, ghid, tutor, duedate, dbglog, ghid2=None,
+                    checkpoint=None):
     """ Pulls the specified repo with pullRepo.sh  """
     args = ['./pullRepo.sh']
 
@@ -28,12 +25,39 @@ def pull_repo(lab, org, ghid, ghid2=None, tutor, duedate, chkpoint=None,
     args.append(REPO_URL.format(_org=org, _repo=args[1]))
 
     #Append shared args
-    args.extends([tutor, lab, duedate[0], duedate[1]])
+    args.extend([tutor, lab, duedate[0], duedate[1]])
 
     #Append checkpoint date if it exists
     if checkpoint is not None:
-       args.extends([chkpoint[0], chekpoint[1]])
+        args.extend([checkpoint[0], checkpoint[1]])
 
     repo_proc = subprocess.Popen(args, stdout=dbglog, stderr=subprocess.PIPE)
-    return (repo-proc.wait() == 0)
+    return (repo_proc.wait() == 0)
+
+def pull_repo(info, student=None, pair=None):
+    """ Pulls the repo specified by student or pair based on
+        the provided information
+    """
+    if student is None and pair is None:
+        logging.error("No student or pair arg supplied.")
+        return False
+
+    args = ["./pullRepo.sh"]
+    if pair is None :
+        args.append(SOLO_NAME.format(_lab=info['labno'], _ghid=student.ghid))
+    else:
+        args.append(PAIR_NAME.format(_lab=info['labno'], _ghid=pair[0].ghid,
+                                     _ghid2=pair[1].ghid))
+    args.append(REPO_URL.format(_org=info['org'], _repo=args[1]))
+
+    #Append shared args
+    args.extend([info['tutor'], info['labno'], info['due_date'][0],
+                 info['due_date'][1]])
+
+    if info['chkpoint'] is not None:
+        args.extend([info['chkpoint'][0], info['chkpoint'][1]])
+
+    repo_proc = subprocess.Popen(args, stdout=info['debug'],
+                                 stderr=subprocess.PIPE)
+    return (repo_proc.wait() == 0)
 
